587ffed2009cfb81bef78dd8b401efcc331a7c62
Kalani Sanidad <ksanidad@hawaii.edu>
2019-05-18 11:44:25 -0700

    add shop points function

--- monsterRPG_to_HaroClicker/component.js
+++ monsterRPG_to_HaroClicker/component.js
@@ -4,7 +4,17 @@ new Vue({
     totalHaro: 0,
     haroPerClick:1,
     tutorial:true,
-    width:100
+    width:100,
+    clicked:{
+      age:true
+    },
   },
+  methods:{
+    clickCalculated:function(){
+      this.clicked.age = !this.clicked.age
+      this.totalHaro -= 5
+      this.haroPerClick += 2
+    },
+  }
 
 })
