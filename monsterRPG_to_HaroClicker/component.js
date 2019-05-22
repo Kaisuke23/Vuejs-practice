0efcbc49a62f6fd3131635f2a2d6a07ab32ebe0a
Kalani Sanidad <ksanidad@hawaii.edu>
2019-05-21 20:20:04 -0700

    add adding function and upgrade function

--- monsterRPG_to_HaroClicker/component.js
+++ monsterRPG_to_HaroClicker/component.js
@@ -5,16 +5,35 @@ new Vue({
     haroPerClick:1,
     tutorial:true,
     width:100,
+    timer: 0,
+    runner:true,
     clicked:{
-      age:true
+      age:true,GGundam: true,
     },
   },
+  watch:{
+    runner(value){
+      let haro = this
+
+      if(!this.runner){
+        setInterval(function(){ haro.totalHaro++ }, this.timer);
+      }
+    }
+  },
   methods:{
-    clickCalculated:function(){
-      this.clicked.age = !this.clicked.age
-      this.totalHaro -= 5
+    clickCalculated(amount,adder,position){
+      this.clicked[position] = !this.clicked[position]
+      this.totalHaro -= amount
       this.haroPerClick += 2
     },
+    runnerChanger(){
+      this.runner = !this.runner
+      this.upgrade(5,1000)
+    },
+    upgrade(howMuch,howFast){
+      this.totalHaro -= howMuch
+      this.timer = howFast
+    }
   }
 
 })
